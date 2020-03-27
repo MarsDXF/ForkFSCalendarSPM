045ea458ce97c7473af69fb0828ce77f156ee268
Vasiliy <Freeneos@icloud.com>
2020-03-27 17:09:08 +0300

    Calendar

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "ForkFSCalendarSPM",
+    platforms: [
+        .iOS(.v11)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -19,8 +22,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "ForkFSCalendarSPM",
+            name: "ObjFSCalendar",
             dependencies: []),
+        .target(
+            name: "ForkFSCalendarSPM",
+            dependencies: ["ObjFSCalendar"]),
         .testTarget(
             name: "ForkFSCalendarSPMTests",
             dependencies: ["ForkFSCalendarSPM"]),
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # ForkFSCalendarSPM
 
-A description of this package.
+This is my fork from the [FSCalendar](https://github.com/WenchaoD/FSCalendar) repository
+
+Supports iOS 11+
+
+## Installation
+
+ForkFSCalendarSPM is available as a [Swift Package Manager](https://swift.org/package-manager/):
